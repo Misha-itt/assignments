6de6377bff0e2323cc7f4a89bef454c3ef5ab96b
Misha Agrawal <misha.agrawal@intimetec.com>
2026-03-26 17:18:49 +0530

    Week-1 Assignment

--- Project/obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d00b43cfad13a0eebb5a63ce35dd2d4a7575e716")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
